71dc2772eb18cd95dbc50b63907ba63e431b174c
azula9713 <nnviduranga@gmail.com>
2021-06-18 19:53:58 +0530

    Removed search bar

--- admin-frontend/src/pages/Students.js
+++ admin-frontend/src/pages/Students.js
@@ -183,29 +183,7 @@ function Students() {
       <PageTitle>Students</PageTitle>
       <div className="mb-4 flex justify-between items-center">
         <div className="flex-1 pr-4 mb-4">
-          <div className="relative max-w-md">
-            <input
-              type="search"
-              className="w-full pl-10 pr-4 py-2 bg-white dark:bg-gray-700 rounded-lg shadow focus:outline-none focus:shadow-outline text-gray-600 font-medium placeholder-gray-600 dark:placeholder-gray-400"
-              placeholder="Search..."
-            />
-            <div className="absolute top-0 left-0 inline-flex items-center p-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6 text-gray-400"
-                viewBox="0 0 24 24"
-                strokeWidth={2}
-                stroke="currentColor"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <rect x={0} y={0} width={24} height={24} stroke="none" />
-                <circle cx={10} cy={10} r={7} />
-                <line x1={21} y1={21} x2={15} y2={15} />
-              </svg>
-            </div>
-          </div>
+          <div className="relative max-w-md"></div>
         </div>
 
         {/* <div className="flex-1 pr-4 mb-4">
