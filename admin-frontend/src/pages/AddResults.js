c3a9206e3e13817fdac4cb0804c3459c5e2c1cc5
azula9713 <nnviduranga@gmail.com>
2021-07-27 22:13:59 +0530

    Minor Fixes

--- admin-frontend/src/pages/AddResults.js
+++ admin-frontend/src/pages/AddResults.js
@@ -285,7 +285,6 @@ const AddResults = () => {
                   <TableCell>
                     <Input
                       type="number"
-                      value={marks}
                       onChange={(e) => {
                         setMarks(e.target.value);
                       }}
