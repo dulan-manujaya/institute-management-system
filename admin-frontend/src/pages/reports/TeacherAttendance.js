aed6ba7bf48d578c425e7c14979c64fb196f14df
Dulan Manujaya <dulan69@gmail.com>
2021-07-27 08:59:03 +0530

    Added teacher filter to teacher attendance report

--- admin-frontend/src/pages/reports/TeacherAttendance.js
+++ admin-frontend/src/pages/reports/TeacherAttendance.js
@@ -30,6 +30,7 @@ const TeacherAttendance = () => {
   const [fromDate, setFromDate] = useState(new Date("10/23/2015"));
   const [toDate, setToDate] = useState(new Date());
   const [teacherId, setTeacherId] = useState("All");
+  const [teacherName, setTeacherName] = useState("All");
 
   const resultsPerPage = 10;
 
@@ -55,7 +56,7 @@ const TeacherAttendance = () => {
       console.log(attendance);
       if (teacherId != "All") {
         attendance.data = attendance.data.filter(
-          (attendance) => attendance.student_id == teacherId
+          (attendance) => attendance.teacher_id == teacherId
         );
       }
       setAttendanceData(
@@ -110,9 +111,14 @@ const TeacherAttendance = () => {
     doc.setFontSize(16);
     doc.text(`From Date : ${fromDate.toISOString().substring(0, 10)}`, 15, 30);
     doc.text(`To Date : ${toDate.toISOString().substring(0, 10)}`, 15, 40);
-    doc.text(`Total Days : ${attendanceResponse.length}`, 15, 50);
+    doc.text(
+      `Teacher : ${teacherName == null ? teacherId : teacherName}`,
+      15,
+      50
+    );
+    doc.text(`Total Days : ${attendanceResponse.length}`, 15, 60);
     doc.autoTable(col, rows, {
-      startY: 60,
+      startY: 70,
       didDrawPage: function (data) {
         doc.setFontSize(10);
         var pageSize = doc.internal.pageSize;
@@ -154,6 +160,33 @@ const TeacherAttendance = () => {
               setToDate(new Date(e.target.value));
             }}
           />
+        </div>
+        <div>
+          <SectionTitle>Teacher</SectionTitle>
+          <Select
+            className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
+            onChange={(e) => {
+              setTeacherId(e.target.value);
+              setTeacherName(
+                e.target.options[e.target.selectedIndex].getAttribute(
+                  "teacher_name"
+                )
+              );
+            }}
+          >
+            <option key={"-1"}>All</option>
+            {!teachers
+              ? null
+              : teachers.map((teacher, i) => (
+                  <option
+                    key={teacher.teacher_id}
+                    value={teacher.teacher_id}
+                    teacher_name={`${teacher.first_name} ${teacher.last_name}`}
+                  >
+                    {teacher.first_name} {teacher.last_name}
+                  </option>
+                ))}
+          </Select>
         </div>
       </div>
       <Button onClick={() => getAllAttendance()} className="my-4 w-1/4">
